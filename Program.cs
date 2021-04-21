ddc7d064c4a40019b91d38b6d647bbe882f08a60
alanisaias24147 <78932267+alanisaias24147@users.noreply.github.com>
2021-04-21 07:34:56 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace alumnos
+{
+    class Persona
+    {
+        string nombre;
+        public string apellido;
+
+        public string nombreCompleto
+         {
+            get => this.nombre + " " + this.apellido;
+         }
+
+        private string colorFavorito;
+
+        public string ColorFavorito
+        {
+            get => this.colorFavorito;
+            set => this.colorFavorito = value;
+        }
+        public Persona(string nombre, string apellido)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+        }
+
+       
+
+        public void presentarse()
+        {
+            Console.WriteLine("hola mi nombre es" + this.nombreCompleto);
+        }
+        
+    }
+}
